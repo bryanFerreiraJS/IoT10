7f9b90f5ea90c7b672062fb50ebf7c67a1399211
Renaudfradin <renaudfradin@gmail.com>
2021-07-20 16:48:49 +0200

    call api

---
+++ src/components/Weather.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="font-medium text-gray-400 mb-5">
+        <h1>weather</h1>
+    </div>
+    <div>
+        <img src="" alt="">
+        <p>{{ getweather }} °C</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name:'Weather',
+    data(){
+        return {
+            getweather: undefined,
+        }
+    },
+    mounted(){
+        axios.get('https://api.openweathermap.org/data/2.5/weather?q=montreuil&units=metric&lang=fr&appid=4905eac944022ae6bb02d2b49f21a9c7')
+        .then(reponse =>{
+            //console.log(reponse.data);
+            this.getweather = reponse.data.main.temp.toFixed();
+            console.log(reponse.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="font-medium text-gray-400 mb-5">Dashboard</div>
   <EconomieTotale />
+  <Weather />
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import EconomieTotale from '@/components/EconomieTotale.vue'
+import Weather from '../components/Weather.vue';
 
 export default defineComponent({
-  components: { EconomieTotale },
+  components: { 
+    EconomieTotale,
+    Weather
+  }
 })
 </script>
