39f1bc70c3ba4e64ae31069ff971981ab04aba8b
jackytruonghetic <74652548+jackytruonghetic@users.noreply.github.com>
2021-07-20 23:20:59 +0200

    fix TotalSaving (#9)
    * fix TotalSaving
    
    * Update src/components/TotalSavings.vue
    
    Co-authored-by: bryanFerreiraJS <42151261+bryanFerreiraJS@users.noreply.github.com>
    
    * fix class TotalSaving
    
    * fix class TotalSaving
    
    Co-authored-by: bryanFerreiraJS <42151261+bryanFerreiraJS@users.noreply.github.com>

--- src/components/TotalSavings.vue
+++ src/components/TotalSavings.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-white shadow-lg flex p-4 flex-col rounded-2xl w-3/4">
     <div class="text-2xl text-black-600 font-bold">Economie Totale</div>
-    <div class="mt-2.5 w-full flex flex-row">
+    <div class="mt-2.5 w-full flex list-total-savings">
       <div class="flex flex-col w-full mr-2 bg-white shadow-md p-4 items-center justify-center text-left rounded-2xl">
         <h3 class="w-full text-lg text-black-600 font-bold">Consommation d'énergie</h3>
         <div class="mt-3 w-full text-black-600">
@@ -9,21 +9,25 @@
           <span class="ml-2 text-base">d’énergie économisée</span>
         </div>
         <div class="mt-3 w-full">
-          <span class="text-xl text-green-400">+</span>
-          <span class="text-xl ml-2 text-green-400">3%</span>
+          <span class="text-xl text-green-400">
+            <span class="text-xl mr-2 text-green-400 clear-margin">+</span>
+            3%
+          </span>
           <span class="text-sm ml-3 text-gray-400">par rapport à la semaine dernière</span>
         </div>
       </div>
 
-      <div class="flex flex-col w-full ml-2 bg-white shadow-md p-4 items-center justify-center text-left rounded-2xl">
+      <div class="flex flex-col w-full ml-2 bg-white shadow-md p-4 items-center justify-center text-left rounded-2xl clear-margin total-savings-col2">
         <h3 class="w-full text-lg text-black-600 font-bold">Budgétaire</h3>
         <div class="mt-3 w-full text-black-600">
           <span class="text-6xl font-bold">1145€</span>
           <span class="ml-2 text-base">de budget économisé</span>
         </div>
         <div class="mt-3 w-full">
-          <span class="text-xl text-green-400">+</span>
-          <span class="text-xl ml-2 text-green-400">5%</span>
+          <span class="text-xl text-green-400">
+            <span class="text-xl mr-2 text-green-400 clear-margin">+</span>
+            5%
+          </span>
           <span class="text-sm ml-3 text-gray-400">par rapport à la semaine dernière</span>
         </div>
       </div>
@@ -36,3 +40,20 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({})
 </script>
+
+<style scoped>
+  .list-total-savings{
+    flex-direction: row;
+  }
+  @media screen and (max-width: 830px) {
+    .list-total-savings{
+      flex-direction: column;
+    }
+    .clear-margin{
+      margin: 0;
+    }
+    .total-savings-col2{
+      margin-top: 1rem;
+    }
+  }
+</style>
