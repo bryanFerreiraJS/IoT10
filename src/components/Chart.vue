cf17bdbbd580bb086ffde2a3859dc584c45e9494
bryanFerreiraJS <micotor91@gmail.com>
2021-07-22 00:02:51 +0200

    remove letter

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -24,7 +24,7 @@ export default defineComponent({
     returnChartData() {
       const data = {
         labels: [
-          'Lun',s
+          'Lun',
           'Mar',
           'Mer',
           'Jeu',
