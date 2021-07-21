33040db382aed41f649301a64b21c56135ef5715
Renaudfradin <renaudfradin@gmail.com>
2021-07-21 16:32:40 +0200

    tast budget inté

---
+++ src/components/SavingsBudget.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-white shadow-lg flex p-4 flex-col rounded-2xl w-4/4 lg:w-1/4">
+    <h2 class="text-2xl text-black-600 font-bold">Economie Totale</h2>
+    <div class="mt-2.5 w-full flex list-total-savings">
+      <div class="flex flex-col w-full mr-2 bg-white shadow-md p-4 items-center justify-center text-left rounded-2xl">
+        <div class="mt-3 w-full text-black-600">
+          <span class="text-5xl font-bold">1145€</span>
+          <span class="ml-2 text-base">de budget économisé</span>
+        </div>
+        <div class="mt-3 w-full">
+          <span class="text-xl text-green-400">
+            <span class="text-xl mr-2 text-green-400 clear-margin">+</span>
+            7%
+          </span>
+          <span class="text-sm ml-3 text-gray-400">par rapport à la semaine dernière</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
+
+<style scoped>
+
+</style>
