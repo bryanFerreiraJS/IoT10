e067bcf077d947668b653f5642d8f95207bf9a83
bryanFerreiraJS <42151261+bryanFerreiraJS@users.noreply.github.com>
2021-07-22 00:52:14 +0200

    change day (#18)

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -2,60 +2,68 @@
   <div class='relative'>
     <div v-if='currentWeather'>
       <img
-        :src="'src/assets/images/weather/day/' + currentWeather + '.png '"
-        alt=''
+        :src="'src/assets/images/weather/'+ (isDay ? 'day' : 'night') + '/' + currentWeather + '.png'"
+        alt='Image de la météo actuelle'
         class='ml-5'
       />
     </div>
     <div class='absolute bottom-3 left-11 uppercase text-white'>      
       <div>{{ currentDate }}</div>
-      <div class='text-base'>{{ getWeathedescription }}</div>
-      <div class='font-bold text-4xl'>{{ getWeathertemp }} °C</div>
+      <div class='text-base'>{{ weatherDescription }}</div>
+      <div class='font-bold text-4xl'>{{ weatherTemp }} °C</div>
     </div>
   </div>
 </template>
+
 <script>
-import axios from 'axios';
+import axios from 'axios'
+
 export default {
   name: 'Weather',
   data() {
     return {
-      getWeathertemp: null,
+      weatherTemp: null,
       currentWeather: null,
       currentDate: null,
-      getWeathedescription: null,
-      date: null,
-    };
+      weatherDescription: null,
+      isDay: null,
+    }
   },
   methods: {
     returnCurrentDate() {
-      const current = new Date();
+      const current = new Date()
       const dateLocale = current.toLocaleString('fr-FR', {
         year: 'numeric',
         month: 'long',
         day: 'numeric',
-      });
-      return dateLocale;
+      })
+      return dateLocale
     },
+    returnIsDay(sunrise, sunset) {
+      const now = Date.now() / 100000
+      return (now >= sunrise && now <= sunset) ? true : false
+    }
   },
   mounted() {
     axios
       .get(
         'https://api.openweathermap.org/data/2.5/weather?q=Montreuil&units=metric&lang=fr&appid=4905eac944022ae6bb02d2b49f21a9c7'
       )
-      .then((reponse) => {
-        this.getWeathertemp = reponse.data.main.temp.toFixed();
-        this.getWeathedescription = reponse.data.weather[0].description;
-        this.currentWeather = reponse.data.weather[0].main;
-      });
-    this.currentDate = this.returnCurrentDate();
+      .then((response) => {
+        this.isDay = this.returnIsDay(response.data.sys.sunrise, response.data.sys.sunset)
+        this.weatherTemp = response.data.main.temp.toFixed()
+        this.weatherDescription = response.data.weather[0].description
+        this.currentWeather = response.data.weather[0].main
+      })
+    this.currentDate = this.returnCurrentDate();``
   },
 };
 </script>
 <style scoped>
-  img{
+  img {
     width: 18.4rem;
   }
+
   @media screen and (max-width: 1100px) {
     img{
       width: auto;
