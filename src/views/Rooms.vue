ee6d8bb3aa4124f762e298f13b2d065f0d324956
bryanFerreiraJS <micotor91@gmail.com>
2021-07-23 00:04:34 +0200

    refacto

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -1,5 +1,8 @@
 <template>
-  <RoomInformations />
+  <RoomInformations
+    v-for='room in rooms'
+    :room='room'
+  />
 </template>
 
 <script>
@@ -7,6 +10,30 @@ import { defineComponent } from 'vue'
 import RoomInformations from '@/components/RoomInformations.vue'
 
 export default defineComponent({
+  data() {
+    return {
+      rooms : [{
+        'id': 1,
+        'isOpen': true,
+        'nbPeoples': 1,
+        'temperature': 12,
+        'lightIntensity': 'low'
+
+      },{
+        'id': 2,
+        'isOpen': false,
+        'nbPeoples': 0,
+        'temperature': 18,
+        'lightIntensity': 'regular'
+      },{
+        'id': 3,
+        'isOpen': false,
+        'nbPeoples': 0,
+        'temperature': 24,
+        'lightIntensity': 'high'
+      }]
+    }
+  },
   components: { RoomInformations }
 })
 </script>
